72d8c8217af65e3390cf4fb2ea412cf7974535fd
Luan G <35840817+luangrezende@users.noreply.github.com>
2021-05-26 16:40:24 -0300

    upload project

---
+++ Template.Project.Application.Lambda/Controllers/AnvisaController.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.Lambda.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Project.Application.Lambda.Controllers
+{
+    public class AnvisaController
+    {
+        public AnvisaController()
+        {
+
+        }
+
+        public void WriteSomething(ILambdaContext context)
+        {
+            context.Logger.LogLine("Test log!");
+        }
+    }
+}
---
+++ Template.Project.Application/Startup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Template.Project.Application
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Bup Beep!");
+        }
+    }
+}
